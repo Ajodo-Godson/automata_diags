c99cdd4f7867a4f34e0992792c3777321dee17e6
Ajodo Godson <godsonajodo2020@gmail.com>
2025-01-18 19:16:00 -0800

    create dfa from tables

--- automata/backend/grammar/regular_languages/dfa/dfa_mod_algo.py
+++ automata/backend/grammar/regular_languages/dfa/dfa_mod_algo.py
@@ -1,5 +1,6 @@
 from .dfa_mod import DFA
 from .algo.kmp import build_kmp_dfa, kmp_search
+from typing import Dict, Optional
 
 
 def create_dfa_from_pattern(pattern: str, alphabet: set[str]) -> DFA:
@@ -16,6 +17,33 @@ def create_dfa_from_pattern(pattern: str, alphabet: set[str]) -> DFA:
         start_state=start_state,
         accept_states=accept_states,
         sink_state=None,
+    )
+
+
+def create_dfa_from_table(
+    table: Dict[str, Dict[str, str]],
+    start_state: str,
+    accept_states: set[str],
+    alphabet: Optional[set[str]] = None,
+    sink_state: Optional[str] = None,
+) -> DFA:
+    """
+    Build a DFA from a table of transitions.
+    """
+    all_states = set(table.keys())
+    if alphabet is None:
+        alpha_set = set()
+        for trans_dict in table.values():
+            alpha_set |= set(trans_dict.keys())
+        alphabet = alpha_set
+
+    return DFA(
+        states=all_states,
+        alphabet=alphabet,
+        transitions=table,
+        start_state=start_state,
+        accept_states=accept_states,
+        sink_state=sink_state,
     )
 
 
