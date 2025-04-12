a4aa935c01632977cd44b7677cc8d276f023ca3b
Ajodo Godson <godsonajodo2020@gmail.com>
2025-04-12 10:16:55 -0700

    updated app.js

--- automata/frontend/src/App.js
+++ automata/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layout';
-import DFASimulator from './components/DFASimulator';
+import DFASimulator from './components/DFA_components/DFASimulator';
 
 function App() {
   return (
