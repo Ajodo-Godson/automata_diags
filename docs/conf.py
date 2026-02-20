a380184227fb063d9954f5ff93cb9b754610fc45
Godson Ajodo <godsonajodo2020@gmail.com>
2026-02-19 18:02:38 -0800

    update docs version to match pyproject.toml 0.2.6

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'Automata Diags'
 copyright = '2025, Godson Ajodo'
 author = 'Godson Ajodo'
-release = '0.2.1'
+release = '0.2.6'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
