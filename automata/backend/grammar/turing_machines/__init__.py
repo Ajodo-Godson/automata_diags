574c583b3bab5d240405021bd84dda0ab8d29a72
Godson Ajodo <godsonajodo2020@gmail.com>
2026-07-14 13:50:00 -0700

    Export NTM, result types, and exceptions from turing_machines package

--- automata/backend/grammar/turing_machines/__init__.py
+++ automata/backend/grammar/turing_machines/__init__.py
@@ -3,4 +3,11 @@ from .base import TuringMachineBase
 from .standard import TuringMachine
 from .multitape import MultiTapeTuringMachine
 from .multihead import MultiHeadTuringMachine
-
+from .nondeterministic import NondeterministicTuringMachine
+from .result import Configuration, Outcome, RunResult
+from .exceptions import (
+    TuringMachineError,
+    StepLimitExceeded,
+    RejectionException,
+    HaltingException,
+)
