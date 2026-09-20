99b80d084c06a4efe0bf32228bd216ded074d7dd
Godson Ajodo <godsonajodo2020@gmail.com>
2026-09-20 02:18:44 -0700

    Stub the ESM-only remark and rehype plugins for tests

---
+++ automata/frontend/src/__mocks__/remarkPluginStub.js
@@ -0,0 +1,5 @@
+/* No-op stand-in for the ESM-only remark/rehype plugins. See reactMarkdownStub. */
+const plugin = () => {};
+module.exports = plugin;
+module.exports.default = plugin;
+module.exports.__esModule = true;
